5e11b53801b5e352685034520491431f217050ba
AvadaKedavra6 <146797901+AvadaKedavra6@users.noreply.github.com>
2024-07-15 19:41:45 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,93 @@
+var data = [
+  {
+    id: 1,
+    question: "Who were Harry's parents?",
+    a: "Henry and Maggie Potter",
+    b: "William and Elizabeth Potter",
+    c: "James and Lily Potter",
+    d: "Voldemort",
+    answer: 3
+  },
+  {
+    id: 2,
+    question: "In order of birth, who are the seven Weasley siblings?",
+    a: "Percy, Bill, George, Charlie, Fred, Ron, Ginny",
+    b: "George, Fred, Percy, Bill, Charlie, Ron, Ginny",
+    c: "Bill, Charlie, Percy, Fred, George, Ron, Ginny",
+    d: "Charlie, Bill, Percy, Fred, George, Ron, Ginny",
+    answer: 3
+  },
+  {
+    id: 3,
+    question: "What are the three Unforgivable Curses?",
+    a: "Impedimenta, Incarcerous and Incendio",
+    b: "Avada Kedavra, Crucio and Imperio",
+    c: "Expelliarmus, Expecto Patronum and Diffindo",
+    d: "Rictusempra, Crucio and Flipendo",
+    answer: 2
+  },
+  {
+    id: 4,
+    question: "Which of these Hogwarts professors teaches Transfiguration?",
+    a: "Snape",
+    b: "Sprout",
+    c: "McGonagall",
+    d: "Voldemort",
+    answer: 3
+  },
+  {
+    id: 5,
+    question: "At the end of Harry Potter and the Sorcerer's Stone, which professor removes his turban to reveal Voldemort on the back of his head?",
+    a: "Quirrel",
+    b: "Snape",
+    c: "Lockhart",
+    d: "Hagrid",
+    answer: 1
+  },
+  {
+    id: 6,
+    question: "Who first shows Harry the diary of Tom Riddle?",
+    a: "Moaning Myrtle",
+    b: "Nearly Headless Nick",
+    c: "Ethernet",
+    d: "Fawkes",
+    answer: 1
+  },
+
+  {
+    id: 7,
+    question: "Who was Tom Marvolo Riddle?",
+    a: "Harry's Father",
+    b: "Harry's Mother",
+    c: "Sirius Black",
+    d: "Voldemort",
+    answer: 4
+  },
+  {
+    id: 8,
+    question: "What's the only book in the Harry Potter series that doesn't feature Lord Voldemort?",
+    a: "The Sorcerer's Stone",
+    b: "The Prisoner of Azkaban",
+    c: "The Deathly Hallows",
+    d: "IDK",
+    answer: 2
+  },
+  {
+    id: 9,
+    question: "Who was the Prisoner of Azkaban, and was he guilty of the crime he was accused of committing?",
+    a: "James Potter and not guilty",
+    b: "Cornelius Fudge and guilty",
+    c: "Sirius Black and not guilty",
+    d: "Rubeus Hagrid and guilty",
+    answer: 3
+  },
+  {
+    id: 10,
+    question: "Who is the Half-Blood Prince?",
+    a: "Voldemort",
+    b: "Dumbledore",
+    c: "Severus Snape",
+    d: "Owl of Harry",
+    answer: 3
+  }
+];
